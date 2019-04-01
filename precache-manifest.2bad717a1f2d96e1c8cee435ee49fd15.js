67bb079b55fbd82e542e263ab01776c2479224a6
woojin <verecahill@gmail.com>
2019-04-01 14:13:35 +0900

    Updates

---
+++ precache-manifest.2bad717a1f2d96e1c8cee435ee49fd15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "1f8ae777d6906d2b2941",
+    "url": "/movie_app/static/js/main.1b7d77bb.chunk.js"
+  },
+  {
+    "revision": "3208e5b0e33a6b03ef06",
+    "url": "/movie_app/static/js/2.3dbb8b67.chunk.js"
+  },
+  {
+    "revision": "1f8ae777d6906d2b2941",
+    "url": "/movie_app/static/css/main.4d67edba.chunk.css"
+  },
+  {
+    "revision": "4fcd02623e80723efba2b2242da2a0cc",
+    "url": "/movie_app/index.html"
+  }
+];
